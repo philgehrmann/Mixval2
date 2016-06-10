ee884e32482b7218c28612021cb39d3128c921eb
Philipp Gehrmann <philipp.gehrmann@sevenval.com>
2016-06-10 17:46:26 +0200

    master - last version

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ var mixVal = {
     waveform_url: '',
     artwork_url:'',
     genre:'',
+    time :''
   },
 
   _Deck2 : {
@@ -41,12 +42,14 @@ var mixVal = {
     waveform_url: '',
     artwork_url:'',
     genre:'',
+    time:''
   },
 
   init : function(){
 
   // mixVal._checkAuth();
   mixVal._resizeVideo();
+  mixVal._aktivateBtn(true);
   },
 
   _listener : function(){
@@ -62,6 +65,12 @@ var mixVal = {
  var playBtn = document.querySelectorAll('.pause.two')[0];
 playBtn.addEventListener("click" , mixVal.pauseDeck2);
 
+var playBtn = document.querySelectorAll('.cue.one')[0];
+playBtn.addEventListener("click" , mixVal.stopDeck1);
+
+var playBtn = document.querySelectorAll('.cue.two')[0];
+playBtn.addEventListener("click" , mixVal.stopDeck2);
+
 
   document.querySelector('.deck.one .fader input').addEventListener('change', function() {
     mixVal._song1.setVolume(this.value);
@@ -76,12 +85,14 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
   setDeck1 : function(sound){
     soundID = sound.attr('data-track');
     SC.get('/tracks/'+soundID).then(function(track) {
+      console.log(track);
       mixVal._Deck1.ID = track.id;
       mixVal._Deck1.title = track.title;
       mixVal._Deck1.waveform_url = track.waveform_url;
       mixVal._Deck1.artwork_url = track.artwork_url;
       mixVal._Deck1.genre = track.genre;
       mixVal._Deck1.stream_url = track.stream_url;
+      mixVal._Deck1.time = track.duration;
       if(mixVal._song1 != ''){
         mixVal._song1.destroy();
         mixVal._song1 = new SoundItem(mixVal._Deck1.ID);
@@ -96,7 +107,7 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
     var wave = document.querySelector('#player1 .wave');
     var title = document.querySelector('#player1 .title');
     var wheel = document.querySelector('#player1 .wheel');
-    wave.innerHTML = "<img src='"+mixVal._Deck1.waveform_url+"'/>";
+    wave.innerHTML = "<div class='pointer'></div><img src='"+mixVal._Deck1.waveform_url+"'/>";
     title.innerHTML = mixVal._Deck1.title;
     wheel.innerHTML = "<img src='"+mixVal._Deck1.artwork_url+"'/>";
 
@@ -126,7 +137,7 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
       var wave = document.querySelector('#player2 .wave');
       var title = document.querySelector('#player2 .title');
       var wheel = document.querySelector('#player2 .wheel');
-      wave.innerHTML = "<img src='"+mixVal._Deck2.waveform_url+"'/>";
+      wave.innerHTML = "<div class='pointer'></div><img src='"+mixVal._Deck2.waveform_url+"'/>";
       title.innerHTML = mixVal._Deck2.title;
       wheel.innerHTML = "<img src='"+mixVal._Deck2.artwork_url+"'/>";
 
@@ -135,22 +146,41 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
 
   playDeck1 : function(){
     $("#player1 .wheel").addClass('spin');
+    $('#player1 .pointer').animate({
+      width: "100%"
+    },mixVal._Deck1.time);
     mixVal._song1.play();
   },
 
   playDeck2 : function(){
     $("#player2 .wheel").addClass('spin');
+    $('#player2 .pointer').animate({
+      width: "100%"
+    },mixVal._Deck2.time);
+    $("#player2 .wheel").addClass('spin');
     mixVal._song2.play();
   },
 
   pauseDeck1 : function(){
+    $('#player1 .pointer').stop();
     $("#player1 .wheel").removeClass('spin');
     mixVal._song1.pause();
   },
 
   pauseDeck2 : function(){
+    $('#player1 .pointer').stop();
     $("#player2 .wheel").removeClass('spin');
     mixVal._song2.pause();
+  },
+
+  stopDeck1 : function(){
+    $("#player1 .wheel").removeClass('spin');
+    mixVal._song1.stop();
+  },
+
+  stopDeck2 : function(){
+    $("#player2 .wheel").removeClass('spin');
+    mixVal._song2.stop();
   },
 
 
@@ -168,26 +198,12 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
 
   _aktivateBtn : function(val){
     var logger = document.getElementById('logger');
-    if (val === true){
       logger.classList.add("active");
       logger.addEventListener("click" , mixVal._scAuth);
-    } else{
-      logger.removeEventListener("click" , mixVal._scAuth);;
-      logger.classList.remove("active");
-    }
+
   },
 
 
-
-
-  _checkUsername : function(){
-   var UserInput = document.getElementsByClassName('username')[0];
-   if(UserInput.value.length > 4){
-     mixVal._aktivateBtn(true);
-   } else{
-     mixVal._aktivateBtn(false);
-   }
-  },
 
 
   _resizeVideo : function(){
@@ -197,7 +213,7 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
       mute: true,
       repeat: true,
       wrapperZIndex: 0,
-      start: 15
+      start: 20
     });
     $("#vid").css({"width": window.innerWidth + "px"});
     $("#vid").css({"height": window.innerHeight + "px"});
@@ -261,7 +277,7 @@ playBtn.addEventListener("click" , mixVal.pauseDeck2);
 
     var TrackitemWrapper = document.getElementsByClassName('tracklist')[0];
     for (var song in tracks){
-      TrackitemWrapper.innerHTML += '<div class="track-item"><p class="name">'+tracks[song].title+'</p><img class="cover" src="'+tracks[song].artwork_url+'" /><div class="infos"><p class="artist"></p></div><p class="bpm">'+tracks[song].bpm+'</p><p data-track="'+tracks[song].id
+      TrackitemWrapper.innerHTML += '<div class="track-item"><p class="name">'+tracks[song].title+'</p><img class="cover" src="'+tracks[song].artwork_url+'" /><div class="infos"></div><p data-track="'+tracks[song].id
 +'"class="one deck" onclick="mixVal.setDeck1($(this));">Deck1</p><p onclick="mixVal.setDeck2($(this));"data-track="'+tracks[song].id+'" class="two deck">Deck2</p></div>';
     }
     TrackitemWrapper.innerHTML = myTracks;
@@ -294,6 +310,7 @@ $(function(){
 var SoundItem = (function(trackID){
 
   var _self = this;
+  var tabwert = 1;
   var trackID = trackID;
   var context, gainNode, filter, reverb, audio, source, url;
 
@@ -309,17 +326,22 @@ var SoundItem = (function(trackID){
   this.createNewSound = function(){
     context = new AudioContext();
     gainNode = context.createGain();
-    filter = context.createBiquadFilter();
+    filterLPF = context.createBiquadFilter();
+    filterHPF = context.createBiquadFilter();
     reverb = context.createConvolver();
     audio = new Audio();
     source;
     url ='https://api.soundcloud.com/tracks/'+trackID+'/stream?client_id=df0870cdaf901a641c6a00c36a0855f9';
+
   }
 
   this.setOutput = function(){
       source.connect(gainNode);
       gainNode.gain.value = 1;
-      gainNode.connect(filter);
+      gainNode.connect(filterLPF);
+      filterLPF.connect(filterHPF);
+      filterHPF.connect(context.destination);
+
     }
   this.setVolume = function(aktValue){
       gainNode.gain.value = aktValue / 127;
@@ -332,13 +354,68 @@ var SoundItem = (function(trackID){
   this.pause = function(){
     source.mediaElement.pause();
   }
+  this.stop = function(){
+    source.mediaElement.disconnect();
+  }
 
   this.setFilterLow = function(val){
+    if(val){
+      var freq = val * 100;
+    } else{
+      freq = 20000;
+    }
+    filterLPF.type = 'lowpass';
+    filterLPF.frequency.value = freq;
 
-    filter.type = 'allpass';
-    filter.frequency.value = val;
-    filter.connect(context.destination);
   }
+
+  this.setFilterHigh = function(val){
+    if(val){
+      var freq = val * 100;
+    } else{
+      freq = 20000;
+    }
+    filterHPF.type = 'highpass';
+    filterHPF.frequency.value = freq;
+  }
+
+  this.pitchSong = function(val){
+    if(val == 64){
+      speed = 1;
+    }
+    else{
+      speed = ((val - 64) * 0.01) + 1;
+    }
+    source.mediaElement.playbackRate = speed;
+  }
+
+  this.pitchSongNew = function(val){
+    var oldSpeed = source.mediaElement.playbackRate;
+    if(tabwert == 1){
+      tabwert = 0;
+      console.log("1");
+      source.mediaElement.playbackRate -= val;
+       _self.rePitch(oldSpeed, 0);
+       }
+  }
+
+  this.rePitch = function(val){
+        setTimeout(function(){
+          console.log(tabwert);
+          source.mediaElement.playbackRate = val;
+          tabwert = 1;
+        },100);
+  }
+  this.pitchSongNewFast = function(val){
+    var oldSpeed = source.mediaElement.playbackRate;
+    if(tabwert == 1){
+      tabwert = 0;
+      source.mediaElement.playbackRate += val;
+       _self.rePitch(oldSpeed, 0);
+       }
+  }
+
+
 
   this.destroy = function(){
     context.close();
@@ -350,6 +427,7 @@ var SoundItem = (function(trackID){
 });
 
 var midi, data;
+var PitchDeck1 = 64;
 
 function checkMidiAccess(){
   if(navigator.requestMIDIAccess){
@@ -385,6 +463,7 @@ function onMIDIMessage(event) {
       // Play
       case 2:
         if (velocity === 127) {
+          $("#player1 .wheel").addClass('spin');
            mixVal._song1.play();
         }
       break;
@@ -392,13 +471,15 @@ function onMIDIMessage(event) {
       // PAUSE
       case 3:
         if (velocity === 127) {
+          $("#player1 .wheel").removeClass('spin');
           mixVal._song1.pause();
         }
       break;
 
       // Filter
       case 4:
-        console.log('Filter auf ' + velocity);
+      console.log('4');
+          mixVal._song1.setFilterLow(velocity);
       break;
 
       // Deck2 Fader
@@ -409,6 +490,7 @@ function onMIDIMessage(event) {
       // Play
       case 6:
         if (velocity === 127) {
+          $("#player2 .wheel").addClass('spin');
            mixVal._song2.play();
         }
       break;
@@ -416,13 +498,83 @@ function onMIDIMessage(event) {
       // PAUSE
       case 7:
         if (velocity === 127) {
+          $("#player2 .wheel").removeClass('spin');
            mixVal._song2.pause();
         }
       break;
 
       // Filter
       case 8:
+      console.log('8');
         mixVal._song2.setFilterLow(velocity);
+      break;
+
+      case 10:
+            console.log('10');
+        mixVal._song2.setFilterHigh(velocity);
+      break;
+
+      case 9:
+            console.log('9');
+        mixVal._song1.setFilterHigh(velocity);
+      break;
+
+      case 11:
+        mixVal._song1.pitchSong(velocity);
+        PitchDeck1 = velocity;
+
+
+      break;
+
+      case 12:
+        mixVal._song2.pitchSong(velocity);
+      break;
+
+      case 13:
+      var rech = ((PitchDeck1 - 64) * 0.01) + 1;
+
+      if(velocity = 127){
+        mixVal._song1.pitchSongNew(rech - 0.05);
+      }else{
+        mixval._song1.pitchSongNew(rech);
+      }
+
+      break;
+
+      case 14:
+
+      var rech = ((PitchDeck1 - 64) * 0.01) + 1;
+
+      if(velocity = 127){
+        mixVal._song1.pitchSongNewFast(rech - 0.05);
+      }else{
+        mixval._song1.pitchSongNewFast(rech);
+      }
+
+      break;
+
+      case 15:
+
+      var rech = ((PitchDeck1 - 64) * 0.01) + 1;
+
+      if(velocity = 127){
+        mixVal._song2.pitchSongNew(rech - 0.05);
+      }else{
+        mixval._song2.pitchSongNew(rech);
+      }
+
+      break;
+
+      case 16:
+
+      var rech = ((PitchDeck1 - 64) * 0.01) + 1;
+
+      if(velocity = 127){
+        mixVal._song2.pitchSongNewFast(rech - 0.05);
+      }else{
+        mixval._song2.pitchSongNewFast(rech);
+      }
+
       break;
   }
 }
